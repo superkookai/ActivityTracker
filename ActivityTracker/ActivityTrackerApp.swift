c850231f102a3d92ad1315976677dc06b208c5ca
Weerawut <weerawut@live.com>
2024-11-27 08:34:22 +0700

    Initial Commit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by Weerawut Chaiyasomboon on 27/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
